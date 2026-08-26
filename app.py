eda3b103b6861b11f341f74b9e2bcab90c4f7e8a
laejmestradacabrera-tech <laejmestradacabrera@gmail.com>
2026-08-25 21:49:24 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,8 @@
+import streamlit as st
 
+# Configuración básica de la página
+st.set_page_config(page_title="Equipa Tus Pasos", layout="wide")
+
+# Un mensaje de prueba
+st.title("🚧 Construyendo Equipa Tus Pasos...")
+st.write("Si ves este mensaje, la conexión a la nube es exitosa.")
